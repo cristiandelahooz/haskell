13283bd2b3a6df58a40b2cadbe859764afcd226c
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-21 08:24:46 -0400

    feat: add playground

---
+++ playground/transmit.hs
@@ -0,0 +1,40 @@
+import Data.Char
+
+type Bit = Int
+
+bin2int :: [Bit] -> Int
+bin2int = foldr (\x y -> x + 2 * y) 0
+
+int2bin :: Int -> [Bit]
+int2bin 0 = []
+int2bin n = n `mod` 2 : int2bin (n `div` 2)
+
+make8 :: [Bit] -> [Bit]
+make8 bits = take 8 (bits ++ repeat 0)
+
+-- Transmission
+
+encode :: String -> [Bit]
+encode = concat . map (make8 . int2bin . ord)
+
+chop8 :: [Bit] -> [[Bit]]
+chop8 [] = []
+chop8 bits = take 8 bits : chop8 (drop 8 bits)
+
+decode :: [Bit] -> String
+decode = map (chr . bin2int) . chop8
+
+transmit :: String -> String
+transmit = decode . channel . encode
+
+channel :: [Bit] -> [Bit]
+channel = id
+
+msg = "Buenos dias. Saludos y buenos dias a todos. Todo esta chevere"
+
+main = do
+  print msg
+  let codi = encode msg
+  print codi
+  let decodi = decode codi
+  print decodi
