7954b486533b20cb2c4a8138304869dd61b54010
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-21 08:21:52 -0400

    feat(compiler): add simple mathematical expersions executor

--- compiler/compiler2.hs
+++ compiler/compiler2.hs
@@ -6,16 +6,17 @@ instance Show Expr where
 
 type Cont = [Op]
 
-data Op = EVAL Expr | ADD Int | MUL Int
+data Op = EVAL_ADD Expr | EVAL_MUL Expr | ADD Int | MUL Int
 
 eval :: Expr -> Cont -> Int
 eval (Val n) c = exec c n
-eval (Add x y) c = eval x (EVAL y : c)
-eval (Mul x y) c = eval x (EVAL y : c)
+eval (Add x y) c = eval x (EVAL_ADD y : c)
+eval (Mul x y) c = eval x (EVAL_MUL y : c)
 
 exec :: Cont -> Int -> Int
 exec [] n = n
-exec (EVAL y : c) n = eval y (MUL n : c)
+exec (EVAL_ADD y : c) n = eval y (ADD n : c)
+exec (EVAL_MUL y : c) n = eval y (MUL n : c)
 exec (ADD n : c) m = exec c (n + m)
 exec (MUL n : c) m = exec c (n * m)
 
@@ -30,26 +31,6 @@ e2 :: Expr
 e2 = Mul (Add (Val 4) (Val 2)) (Val 3)
 
 main = do
-  print e1
-  let v1 = eval e1 []
-  print v1
-  let w1 = exec [(ADD 5)] 3
-  print w1
-  let z1 = exec [EVAL e1, ADD 5] 1
-  print z1
-  let t1 = value e1
-  print t1
-
-  print ex
-  let vx = eval ex [EVAL e1]
-  print vx
-  let wx = exec [ADD 7] 5
-  print wx
-  let zx = exec [EVAL ex, ADD 3] 1
-  print zx
-  let tx = value ex
-  print tx
-
   print e2
   let v2 = eval e2 []
   print v2
